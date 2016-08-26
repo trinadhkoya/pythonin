1da7287f2206784c4c7e5d93115dff9fc550da73
androidmates <trinadhkoya@outlook.com>
2016-08-26 13:28:14 +0530

    initial commit message

--- HelloWorld/views.py
+++ HelloWorld/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+def sayHelloWorld(request):
+    text = """<h1>Hello ,Welcome to Django World</h1>"""
+    return HttpResponse(text)
